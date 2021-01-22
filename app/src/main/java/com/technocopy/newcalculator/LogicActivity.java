639cbb9d06d0e9f5667c957f8c6ad2517cc9ec33
Sergi <technocopy@mail.ru>
2021-01-22 10:34:12 +0300

    Android1 Lesson4 1. The begining.

---
+++ app/src/main/java/com/technocopy/newcalculator/LogicActivity.java
@@ -0,0 +1,14 @@
+package com.technocopy.newcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logic);
+    }
+}
